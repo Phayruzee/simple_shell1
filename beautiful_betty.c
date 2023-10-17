d5d8880cfa55f8a8b8460eee02bd132d05a434fa
Phayruzee <faruqadeniran05@gmail.com>
2023-10-17 09:22:19 -0700

    1_shell

---
+++ beautiful_betty.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program.
+ *
+ * Return: 0 if successful, otherwise an error code.
+ *
+ */
+
+int main(void)
+{
+	printf("Hello, Betty!, i can perfeectly beat your problem now\n");
+	return (0);
+}
+
