ad4274d2b25b045ee74050140c7b25a677b6473c
Phayruzee <faruqadeniran05@gmail.com>
2023-10-18 04:15:17 -0700

    1_shell

--- phy_exe.c
+++ phy_exe.c
@@ -12,25 +12,9 @@ void chief_command(const char *bars)
 	}
 	else if (child_pid == 0)
 	{
-		char *args[140]; /* Array to store the command and its arguments */
-		int counter_arg = 0;
-		/* Counter for the number of arguments */
-
-		/*Tokenize the input string to extract the command and its arguments*/
-		char *token = strtok((char *)bars, " ");
-
-		while (token != NULL)
-		{
-			args[counter_arg++] = token;
-			token = strtok(NULL, " ");
-		}
-		args[counter_arg] = NULL;
-		/* Null-terminate the args array */
-		/* Execute the command */
-		execvp(args[0], args);
-
-		/** if execvp fails print an error message.*/
-		fm_print("Problem encounter while executing Bars.\n");
+		/* child process*/
+		execlp(bars, bars, (char *) NULL);
+		perror("problem encounterd");
 		exit(EXIT_FAILURE);
 	}
 	else
